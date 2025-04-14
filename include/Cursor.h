6219ea171f0ac8cde1b3d1b45e7579efe2c450f5
Daniel <victor-daniel.montana@ensta-paris.fr>
2025-04-14 18:54:32 +0200

    Commit with few changes

--- include/Cursor.h
+++ include/Cursor.h
@@ -4,13 +4,6 @@
 #include "Shape.h"
 
 class Cursor : public Shape {
-public:
-    Cursor(float x, float y, int currentPeg, float pegDistance);  // Constructor to set the initial position of the cursor
-    void moveLeft();           // Move the cursor to the left
-    void moveRight();          // Move the cursor to the right
-    void draw(sf::RenderWindow& window) override;  // Draw the cursor
-    void setPosition(float x, float y) override;  // Set the cursor position
-    sf::Vector2f getPosition() const override;    // Get the cursor position
 
 private:
     sf::ConvexShape shape;  // The triangle shape representing the cursor
@@ -18,6 +11,16 @@ private:
     float yPosition;        // Current y position of the cursor
     int currentPeg;       // Current peg index (0, 1, or 2)
     float pegDistance;      // Distance between pegs
+
+public:
+    Cursor(float x, float y, int currentPeg, float pegDistance);  // Constructor to set the initial position of the cursor
+
+    void moveLeft();           // Move the cursor to the left
+    void moveRight();          // Move the cursor to the right
+    
+    void draw(sf::RenderWindow& window) override;  // Draw the cursor
+    void setPosition(float x, float y) override;  // Set the cursor position
+    sf::Vector2f getPosition() const override;    // Get the cursor position
 };
 
 #endif // CURSOR_H
